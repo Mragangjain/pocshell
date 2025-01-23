e773d7d5177f52ad9be09ae30f5e3819201d6f57
Mragang Jain <mragangjain@CLPT2057.local>
2025-01-23 16:35:57 +0530

    added css changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,36 @@
-import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  useEffect(() => {
+    // Listener to receive messages from the iframe
+    const handleMessage = (event) => {
+      const allowedOrigin = 'https://vulcan-uat.dev.intouch.capillarytech.com'; // Update with iframe origin
+      if (event.origin !== allowedOrigin) {
+        console.warn('Received message from unauthorized origin:', event.origin);
+        return;
+      }
+
+      // Check if the message contains cookies
+      if (event.data?.type === 'cookies') {
+        const cookies = event.data.data;
+        console.log('Received cookies from iframe:', cookies);
+
+        // Set cookies in the parent application
+        cookies.split(';').forEach((cookie) => {
+          document.cookie = cookie.trim();
+        });
+      }
+    };
+
+    window.addEventListener('message', handleMessage);
+
+    // Cleanup listener on component unmount
+    return () => {
+      window.removeEventListener('message', handleMessage);
+    };
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
      <iframe
